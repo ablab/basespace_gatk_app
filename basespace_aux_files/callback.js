2f736cdc81c7b9c2b68a6e07165a3c7d3fc5c5a8
Alexey Gurevich <alexeigurevich@gmail.com>
2015-11-18 15:00:33 +0300

    Cosmetic changes in basespace_aux files

--- basespace_aux_files/callback.js
+++ basespace_aux_files/callback.js
@@ -2,7 +2,7 @@ function launchSpec(dataProvider)
 {
     return {
               commandLine:  ["python","/pipeline/pipeline.py", "basespace"],
-              containerImageId:"gurevich/gatk3best",
+              containerImageId:"gurevich/gatk3_best_practices",
               Options: ["bsfs.enabled=false"]
         }
 }
